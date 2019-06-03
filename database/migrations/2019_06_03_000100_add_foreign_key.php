13a22782cf891e8ccacf54e5c46cd4755f0558dd
MarkoM91 <markomedic91@gmail.com>
2019-06-03 12:13:55 +0200

    commit1

---
+++ database/migrations/2019_06_03_000100_add_foreign_key.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      $table->foreign('category_id', 'category')
+            ->references('id')
+            ->on('categories')
+            ->onDelete('cascade');
+      $table->foreign('post_id', 'post')
+            ->references('id')
+            ->on('posts')
+            ->onDelete('cascade');
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      schema::table('category_post', function(Blueprint $table) {
+
+        $table->dropForeign('category');
+        $table->dropForeign('post');
+      });
+    }
+}
