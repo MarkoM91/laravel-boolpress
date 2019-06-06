3d5d93a056f778ba81b98976c3c01cd2b715363d
MarkoM91 <markomedic91@gmail.com>
2019-06-06 11:51:24 +0200

    commit26

--- resources/views/page/new-post.blade.php
+++ resources/views/page/new-post.blade.php
@@ -2,34 +2,38 @@
 
 @section('content')
 
-  <div class="edit">
-        <h1>CREATE NEW POST</h1>
-      <form method="post" action="{{ route('save.new.post') }}">
+  <div class="edit-content">
+    <div class="edit">
+          <h1>CREATE NEW POST</h1>
+        <form method="post" action="{{ route('save.new.post') }}">
 
-        @csrf
-        @method('POST')
-        <div>
-          <label for="title">TITLE</label>
-          <input id="input" type="text" name="title" value="">
-        </div>
-        <div>
-          <label for="content">CONTENT</label>
-          <input id="input1" type="text" name="content" value="">
-        </div>
-        <div>
-          <label for="author_id">Author</label>
-          <select id="select" name="author_id">
-            @foreach ($authors as $author)
-              <option value="{{$author->id}}">{{$author->username}}</option>
+          @csrf
+          @method('POST')
+          <div>
+            <label for="title">TITLE</label>
+            <input id="input" type="text" name="title" value="">
+          </div>
+          <div>
+            <label for="content">CONTENT</label>
+            <input id="input1" type="text" name="content" value="">
+          </div>
+          <div>
+            <label for="author_id">Author</label>
+            <select id="select" name="author_id">
+              @foreach ($authors as $author)
+                <option value="{{$author->id}}">{{$author->username}}</option>
+              @endforeach
+            </select>
+          </div>
+          <div>
+            @foreach ($categories as $category)
+              <input type="checkbox" name="categories[]" value="{{ $category -> id }}">{{ $category -> name }}<br> <!-- mi ridà un array di categorie-->
             @endforeach
-          </select>
-        </div>
-        <div>
-          @foreach ($categories as $category)
-            <input type="checkbox" name="categories[]" value="{{ $category -> id }}">{{ $category -> name }}<br> <!-- mi ridà un array di categorie-->
-          @endforeach
-        </div>
-        <button type="submit" name="SAVE NEW CAR">SAVE NEW post</button>
-      </form>
+          </div>
+          <button type="submit" name="SAVE NEW CAR">SAVE NEW post</button>
+        </form>
+    </div>
   </div>
+
+  
 @stop
